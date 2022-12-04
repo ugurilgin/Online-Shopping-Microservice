671e2434b86e9d5b50e707b1bae5bbf77075f5a4
ugurilgin <43095839+ugurilgin@users.noreply.github.com>
2022-12-04 22:27:48 +0300

    Comment

--- microservice-parent/inventory-service/src/main/java/com/ugisoftware/inventoryservice/webapi/controllers/InventoryController.java
+++ microservice-parent/inventory-service/src/main/java/com/ugisoftware/inventoryservice/webapi/controllers/InventoryController.java
@@ -16,6 +16,9 @@ public class InventoryController implements InventoryService {
     {
         this.inventoryService=inventoryService;
     }
+    // http://localhost:8082/api/inventory/iphone-13,iphone13-red
+
+    // http://localhost:8082/api/inventory?skuCode=iphone-13&skuCode=iphone13-red
     @GetMapping
     public List<InventoryResponse> isInStock(@RequestParam List<String> skuCode) {
 
